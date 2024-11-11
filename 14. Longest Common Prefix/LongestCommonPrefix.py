60608e40bc30a70dfe4082d5fa30c677ba21ac8e
Reece Tunesi <rtunesi2000@gmail.com>
2024-11-11 17:02:20 +0000

    Longest Common Prefix Problem - To be Resolved.

---
+++ 14. Longest Common Prefix/LongestCommonPrefix.py
@@ -0,0 +1,17 @@
+# 14. Longest Common Prefix - Leetcode Problem
+
+strs1 = ["flower", "florist", "flow"]   # RETURNS "fl"
+strs2 = ["dog", "racecar", "car"]       # RETURNS ""
+
+prefix = ""
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        for value in strs1:
+            for letter in (value):
+                return prefix
+        
+for word in strs1:
+    print("---")
+    for letter in word:
+        print(letter)
